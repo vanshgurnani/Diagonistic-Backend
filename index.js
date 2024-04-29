f416f9280ca6f28409a8412b40255aa8a1fad594
Vansh Gurnani <gurnanivansh57@gmail.com>
2024-04-29 14:37:46 +0530

    updated index.js part1

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const serverless = require("serverless-http");
 
-
-
 const authRouter = require("./routers/auth_router");
 
 const EXPRESS_SESSION_CONFIGS = {
@@ -25,27 +23,13 @@ app.use(bodyParser.json());
 
 app.use("/api/auth", authRouter);
 
-// to run and test locally
+// This section is for running and testing locally
 if (process.env.DEVELOPMENT) {
     const port = process.env.PORT || 3000;
     app.listen(port, () => {
         console.log(`Server is running on PORT: ${port}`);
     });
 }
-// to run over lambda
-module.exports.handler = async (event, context) => {
-    try {
-        console.log(`[handler] Event received - ${JSON.stringify(event)}`);
-        const path = event.path || "";
-        console.log("[Lambda Handler] Requested Path -", path);
-        return await serverless(app)(event, context);
-    } catch (error) {
-        console.error(`Lambda Handler Error: ${JSON.stringify(error)}`);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                error: error?.message ? error.message : `Internal sever error`,
-            }),
-        };
-    }
-};
+
+// Exporting the handler function for AWS Lambda
+module.exports.handler = serverless(app);
