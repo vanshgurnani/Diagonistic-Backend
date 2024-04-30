41a6a09c237068bcbb7e5ef11d7d3631b672ebd0
Vansh Gurnani <gurnanivansh57@gmail.com>
2024-05-01 01:06:06 +0530

    done

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ app.use(session(EXPRESS_SESSION_CONFIGS));
 
 app.use(passport.initialize());
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000', 
+    'https://diagnostic-frontend.vercel.app']
+}));
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
