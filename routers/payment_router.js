a98dcbf9bf721d1bd8bb997084ce0143110aaa4f
Vansh Gurnani <vansh.gurnani@blackcheriemedia.com>
2024-07-10 22:47:37 +0530

    payment cors done

--- routers/payment_router.js
+++ routers/payment_router.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const cors = require("cors"); // Import cors middleware
 const payment = require("../controllers/payment_handler");
 
+// Apply CORS middleware globally to your router
+router.use(cors());
+
+// Define routes
 router.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Payment Route!" });
 });
 
+router.get("/checkout", payment.checkout);
 
-router.get(
-  "/checkout",
-  payment.checkout
-);
-
-router.post(
-  "/paymentVerification",
-  payment.paymentVerification
-);
+router.post("/paymentVerification", payment.paymentVerification);
 
 // send the razorpay api key
 router.get("/getKey", (req, res) => {
