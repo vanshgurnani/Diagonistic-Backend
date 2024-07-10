79ade87115b0a0d5db229de5e18861b3111bb321
Vansh Gurnani <vansh.gurnani@blackcheriemedia.com>
2024-07-10 20:56:10 +0530

    revoked index.js code

--- index.js
+++ index.js
@@ -32,17 +32,27 @@ const allowedOrigins = [
     "https://diagonistic-backend.vercel.app"
 ];
 
-const corsOptions = {
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["X-Requested-With", "content-type"],
-    credentials: true
-};
-
+// Add this middleware to your server configuration
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 app.use(passport.initialize());
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -67,5 +77,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on PORT: ${port}`);
 });
-
-
