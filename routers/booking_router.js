6fd8cb539c52378ca48c2520bb755820d4f96738
Vansh Gurnani <vansh.gurnani@blackcheriemedia.com>
2024-10-09 16:43:12 +0530

    done

--- routers/booking_router.js
+++ routers/booking_router.js
@@ -5,24 +5,22 @@ const Book = require('../controllers/booking_handler');
 const multer = require('multer');
 
 // Define a custom file filter function
-const fileFilter = (req, file, cb) => {
-    // Check if the uploaded file is a PDF or DOC file
-    if (file.mimetype === 'application/pdf' || 
-        file.mimetype === 'application/msword' || 
-        file.mimetype === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
-        // Accept the file
-        cb(null, true);
-    } else {
-        // Reject the file
-        cb(new Error('Only PDF and DOC files are allowed!'), false);
-    }
-};
+// const fileFilter = (req, file, cb) => {
+//     // Check if the uploaded file is a PDF or DOC file
+//     if (file.mimetype === 'application/pdf' || 
+//         file.mimetype === 'application/msword' || 
+//         file.mimetype === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
+//         // Accept the file
+//         cb(null, true);
+//     } else {
+//         // Reject the file
+//         cb(new Error('Only PDF and DOC files are allowed!'), false);
+//     }
+// };
 
 // Configure multer with the custom file filter and memory storage
 const upload = multer({
-    storage: multer.memoryStorage(),
-    // limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB file size limit
-    fileFilter: fileFilter
+    storage: multer.memoryStorage()
 });
 
 router.post('/', JwtService.validateJwt, Book.createBooking);
